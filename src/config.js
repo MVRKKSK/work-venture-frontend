271d01ccfc7f51b4c4e717e8f4f91ed7b2c49232
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2023-09-30 11:31:38 +0530

    updated hosted url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API = "https://work-venture-backend.herokuapp.com/api";
+export const API = "https://work-venture-backend-kappa.vercel.app/api";
 // https://work-venture-backend.herokuapp.com/api
